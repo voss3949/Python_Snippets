8c5eb4cebddb7c5852b1448ea65658d651f2a273
voss3949 <54409097+voss3949@users.noreply.github.com>
2023-04-05 03:52:49 -0400

    Create NBA_STATS_QUERY.py

---
+++ NBA_STATS_QUERY.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import sqlite3 as sql
+
+df = pd.read_csv('C:\\Users\\12245\\OneDrive\\Documents\\Python_Projects\\Data\\nba_example.tsv', sep='\t')
+
+# df.to_html('C:\\Users\\12245\\OneDrive\\Documents\\Python_Projects\\Data\\nba_to_html.html')
+
+# Connect to the database
+conn = sql.connect('C:\\Users\\12245\\OneDrive\\Documents\\Python_Projects\\Data\\nba_stats_example.db')
+
+# Write the DataFrame to the database as a table
+table_name = 'nba_stats'
+
+df.to_sql(table_name, conn, if_exists='replace')
+
+query = 'SELECT * FROM nba_stats WHERE AST > 6'
+
+df_query = pd.read_sql_query(query, conn)
+
+print(df_query)
+
+# Close the connection
+conn.close()
+
