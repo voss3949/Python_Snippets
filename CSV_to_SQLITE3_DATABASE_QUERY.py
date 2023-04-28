4aee336d286043697db3f9a0516d93e3fdd4c04d
voss3949 <54409097+voss3949@users.noreply.github.com>
2023-04-27 21:07:58 -0400

    Update CSV_to_SQLITE3_DATABASE_QUERY.py

--- CSV_to_SQLITE3_DATABASE_QUERY.py
+++ CSV_to_SQLITE3_DATABASE_QUERY.py
@@ -10,7 +10,7 @@ conn = sql.connect('C:\\Users\\12245\\OneDrive\\Documents\\Python_Projects\\Data
 df1.to_sql('t1', conn, if_exists='replace', index=False)
 df2.to_sql('t2', conn, if_exists='replace', index=False)
 
-result = pd.read_sql_query("SELECT t1.name, t2.department FROM t1 INNER JOIN t2 ON t1.name = t2.name; ", conn)
+result = pd.read_sql_query("SELECT t1.name, t2.department, t1.salary FROM t1 INNER JOIN t2 ON t1.name = t2.name; ", conn)
 
 conn.close()
 
